20a38d2932a4d050e1c527f2ef44db58619f5407
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-04-16 14:25:46 -0500

    many changes to the website

--- dev.bat
+++ dev.bat
@@ -1,11 +1,9 @@
 @echo off
 
-start cmd /k sass --watch public/scss:public/css
-
 start cmd /k http-server -c-1 -p5345
 
-code .
+start cmd /k code .
 
-explorer "localhost:5345"
+start http://localhost:5345
 
 exit
